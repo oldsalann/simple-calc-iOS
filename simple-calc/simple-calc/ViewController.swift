cefa00d4767d6e7b70ac02163c393c16d3db45a6
Nicholas Olds <oldsn@D-10-19-16-169.dhcp4.washington.edu>
2018-10-17 08:53:17 -0700

    added project

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Nicholas Olds on 10/17/18.
+//  Copyright © 2018 Nicholas Olds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
